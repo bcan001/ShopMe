4fd829be50d39f3e1572fc79f5a3bf7f06adff70
Ben Caneba <bcaneba@gmail.com>
2015-08-15 21:24:23 -0700

    added user model

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,27 @@
+class UsersController < ApplicationController
+	def new
+		@user = User.new
+	end
+	def create
+		@user = User.new(user_params)
+		@user.save
+		if @user.save
+			session[:user_id] = @user.id
+			flash[:notice] = "User Successfully created"
+			redirect_to items_path
+		else
+      flash[:notice] = "Please fill in the fields correctly to create a user"
+			render 'new'
+		end
+	end
+	def edit
+	end
+	def update
+	end
+	def destroy
+	end
+	private
+	def user_params
+		params.require(:user).permit(:username, :password, :first_name, :last_name, :email)
+	end
+end
