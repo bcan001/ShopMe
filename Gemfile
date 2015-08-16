271799c724f3f5950219f30a9003a9f8bd17e32a
Ben Caneba <bcaneba@gmail.com>
2015-08-15 21:33:34 -0700

    rspec work

--- Gemfile
+++ Gemfile
@@ -1,7 +1,7 @@
 source 'https://rubygems.org'
 
 
-
+gem 'pry-rails'
 
 gem 'bcrypt'
 
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "when attempting to create a new user" do
+  	it "needs to have all fields filled in on the form" do
+  		user_count = User.all.count
+  		user = User.create(first_name: "Benjamin")
+  		expect(User.all.count).to eq(user_count)
+  	end
+  end
+  
 end
